9258f9db1836be9a002c6feae66f691364a8b7aa
Zuev Akim <beliashkoff@vk.com>
2026-02-02 00:20:58 +0300

    refactor: create user struct

---
+++ internal/domain/user.go
@@ -0,0 +1,19 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID          uuid.UUID `db:"id"`
+	Fname       string    `db:"fname"`
+	Sname       string    `db:"sname"`
+	Tel         string    `db:"tel"`
+	Email       string    `db:"email"`
+	RoleType    string    `db:"role_type"`
+	UtmSource   string    `db:"utm_string"`
+	LastLoginAt time.Time `db:"last_login_at"`
+	CreatedAt   time.Time `db:"created_at"`
+}
